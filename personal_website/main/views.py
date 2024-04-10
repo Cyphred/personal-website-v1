beaa282b89f3de0d61ddf70a582fd3d017ff4a2b
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2024-04-10 10:35:35 +0800

    Adds projects segment

--- personal_website/main/views.py
+++ personal_website/main/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
 def index(request):
-  return render(request, "main/index.html")
+  
+  projects = [
+    {
+      'title': 'Prescriptech',
+      'description': 'An electronic prescription-issuing and consultation system.',
+      'images': [
+        'healthcare-provider-login.jpg',
+        'healthcare-provider-registration.jpg',
+        'new-consultation.jpg',
+        'patient-overview-santos.jpg',
+        'pharmacy-backup-code.jpg',
+        'prescription-editor.jpg',
+      ]
+    },
+  ]
+  
+  return render(request, "main/index.html", {'projects': projects})
   
 def services(request):
   return render(request, "main/services.html") 
